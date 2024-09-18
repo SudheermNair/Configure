5e23e47f82d51d08355c4dbe9f6cd1c0bfaafd35
Aswin U <aswin.u@hudini.io>
2024-09-18 16:02:26 +0530

    poc-initial dev

--- configdemo/src/App.js
+++ configdemo/src/App.js
@@ -1,17 +1,30 @@
-
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import FieldModules from "./components/screens/fieldModules";
+import FieldSelected from "./components/screens/fieldSelected";
+import { useState } from "react";
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState(null);
+  const [selectedDropdowns, setSelectedDropdowns] = useState([]);
+
+  const handleDropdownChange = (value) => {
+    setSelectedDropdowns((prev) => [...prev, value]);
+  };
+
   return (
     <div className="App">
-      <div className='information-container' >
-      <Sidebar/>
-      <Navbar/>
+      <Navbar />
+      <div className="main-content">
+        <Sidebar setActiveComponent={setActiveComponent} />
+        <div className="field-modules-container">
+          {activeComponent === "Configuration" && (
+            <FieldModules onDropdownChange={handleDropdownChange} />
+          )}
+          <FieldSelected selectedDropdowns={selectedDropdowns} />
+        </div>
       </div>
-      
-      
     </div>
   );
 }
