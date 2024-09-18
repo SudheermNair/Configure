04f0e44fe076d90e906af6bc4c65daea42eb4581
Shreyas S <shreyas.s@hudini.io>
2024-09-18 16:30:17 +0530

    Add config - Intial Setup

--- configdemo/src/App.js
+++ configdemo/src/App.js
@@ -1,17 +1,16 @@
-
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import AddConfig from "./components/AddConfig/addConfig";
 
 function App() {
   return (
     <div className="App">
-      <div className='information-container' >
-      <Sidebar/>
-      <Navbar/>
+      <div className="information-container">
+        <Sidebar />
+        <Navbar />
       </div>
-      
-      
+      <AddConfig />
     </div>
   );
 }
---
+++ configdemo/src/components/AddConfig/addConfig.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddConfig() {
+  return <div>addConfig</div>;
+}
+
+export default AddConfig;
