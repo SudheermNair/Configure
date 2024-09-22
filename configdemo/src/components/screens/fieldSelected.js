95f03f1f5b32b0329845cc6d55ddc4d60fd6f690
Shreyas S <shreyas.s@hudini.io>
2024-09-22 22:19:48 +0530

    Added Remove button

--- configdemo/src/components/screens/fieldSelected.js
+++ configdemo/src/components/screens/fieldSelected.js
@@ -101,8 +101,55 @@
 
 // New Code - Working
 
+// import React from "react";
+// import "./styles.scss";
+
+// const FieldSelected = ({ data = [], setData }) => {
+//   const removeItem = (hotelId, moduleName, submoduleName) => {
+//     const updatedData = data
+//       .map((hotel) => {
+//         if (hotel.hotelId === hotelId) {
+//           if (moduleName) {
+//             return {
+//               ...hotel,
+//               modules: hotel.modules.filter((mod) => mod.name !== moduleName),
+//             };
+//           }
+//           return null; // Removes the entire hotel
+//         }
+//         return hotel;
+//       })
+//       .filter(Boolean);
+
+//     setData(updatedData);
+//   };
+
+//   const handleSubmit = () => {
+//     if (data.length === 0) {
+//       alert("No data to submit!");
+//       return;
+//     }
+//     alert("Submitting Data: " + JSON.stringify(data, null, 2));
+//   };
+
+//   if (data.length === 0) {
+//     return null;
+//   }
+
+//   return (
+//     <div className="field-selected">
+//       <h3>Selected Configurations</h3>
+//       <pre>{JSON.stringify(data, null, 2)}</pre> {/* Display JSON-like data */}
+//       <button onClick={handleSubmit}>Submit</button>
+//     </div>
+//   );
+// };
+
+// export default FieldSelected;
+
 import React from "react";
 import "./styles.scss";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const FieldSelected = ({ data = [], setData }) => {
   const removeItem = (hotelId, moduleName, submoduleName) => {
@@ -110,12 +157,48 @@ const FieldSelected = ({ data = [], setData }) => {
       .map((hotel) => {
         if (hotel.hotelId === hotelId) {
           if (moduleName) {
-            return {
-              ...hotel,
-              modules: hotel.modules.filter((mod) => mod.name !== moduleName),
-            };
+            if (submoduleName) {
+              return {
+                ...hotel,
+                modules: hotel.modules.map((mod) => {
+                  if (mod.name === moduleName) {
+                    return {
+                      ...mod,
+                      submodules: mod.submodules.filter(
+                        (sub) => sub.name !== submoduleName
+                      ),
+                    };
+                  }
+                  return mod;
+                }),
+              };
+            } else {
+              return {
+                ...hotel,
+                modules: hotel.modules.filter((mod) => mod.name !== moduleName),
+              };
+            }
           }
-          return null; // Removes the entire hotel
+          return null; // Remove the entire hotel if no moduleName
+        }
+        return hotel;
+      })
+      .filter(Boolean);
+
+    setData(updatedData);
+  };
+
+  const removeKey = (hotelId, key) => {
+    const updatedData = data
+      .map((hotel) => {
+        if (hotel.hotelId === hotelId) {
+          const { [key]: _, ...remainingKeys } = hotel; // Remove the specific key
+          return {
+            ...remainingKeys,
+            hotelId: hotel.hotelId,
+            name: hotel.name,
+            modules: hotel.modules,
+          };
         }
         return hotel;
       })
@@ -139,7 +222,61 @@ const FieldSelected = ({ data = [], setData }) => {
   return (
     <div className="field-selected">
       <h3>Selected Configurations</h3>
-      <pre>{JSON.stringify(data, null, 2)}</pre> {/* Display JSON-like data */}
+      <pre className="selected-json-container">
+        {JSON.stringify(data, null, 2)}
+      </pre>
+      <ul>
+        {data.map((hotel, hotelIndex) => (
+          <li key={hotelIndex}>
+            <div>
+              {`Hotel: ${hotel.name}, ID: ${hotel.hotelId}`}
+              <button
+                className="remove-btn"
+                onClick={() => removeItem(hotel.hotelId)}
+              >
+                <DeleteIcon style={{ fontSize: 18 }} />
+              </button>
+            </div>
+            {hotel.modules.map((module, moduleIndex) => (
+              <div key={moduleIndex}>
+                <span>{`Module: ${module.name}`}</span>
+                <button
+                  className="remove-btn"
+                  onClick={() => removeItem(hotel.hotelId, module.name)}
+                >
+                  <DeleteIcon style={{ fontSize: 18 }} />
+                </button>
+                {module.submodules.map((submodule, submoduleIndex) => (
+                  <div key={submoduleIndex}>
+                    <span>{`Submodule: ${submodule.name}`}</span>
+                    <button
+                      className="remove-btn"
+                      onClick={() =>
+                        removeItem(hotel.hotelId, module.name, submodule.name)
+                      }
+                    >
+                      <DeleteIcon style={{ fontSize: 18 }} />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            ))}
+            {Object.keys(hotel)
+              .filter((key) => !["hotelId", "name", "modules"].includes(key))
+              .map((key) => (
+                <div key={key}>
+                  {`${key}: ${hotel[key]}`}
+                  <button
+                    className="remove-btn"
+                    onClick={() => removeKey(hotel.hotelId, key)}
+                  >
+                    <DeleteIcon style={{ fontSize: 18 }} />
+                  </button>
+                </div>
+              ))}
+          </li>
+        ))}
+      </ul>
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );
