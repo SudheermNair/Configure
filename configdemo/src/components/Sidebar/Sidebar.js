f0587e946c35f804bbcc32a35f007317a0385169
Athira S L <athira.sl@hudini.io>
2024-09-23 17:14:20 +0530

    latest updates

--- configdemo/src/components/Sidebar/Sidebar.js
+++ configdemo/src/components/Sidebar/Sidebar.js
@@ -1,87 +1,77 @@
-import React, { useState } from "react";
-import "./Sidebar.scss";
-import DataObjectIcon from "@mui/icons-material/DataObject";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import ApartmentIcon from "@mui/icons-material/Apartment";
-import PersonIcon from "@mui/icons-material/Person";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import AnalyticsIcon from "@mui/icons-material/Analytics";
-import DiningIcon from "@mui/icons-material/Dining";
-import HudiniLogo from "../../assets/images/hudini-logo-black.png";
+import React, { useState } from 'react';
+import './Sidebar.scss';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import PersonIcon from '@mui/icons-material/Person';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import DiningIcon from '@mui/icons-material/Dining';
+import HudiniLogo from '../../assets/images/hudini-logo-black.png';
 
 function Sidebar({ setIsConfigActive }) {
   const [activeIndex, setActiveIndex] = useState(null);
 
   const handleTileClick = (index) => {
     setActiveIndex(index);
-
-   
-    if (index !== 3) {
-      setIsConfigActive(false);
+    if (index === 3) {
+      setIsConfigActive(true); 
+    } else {
+      setIsConfigActive(false); 
     }
-  };
-
-  const handleConfigClick = () => {
-    setIsConfigActive(true); 
   };
 
   return (
     <div className="Sidebar">
       <div className="image-container">
-      <img
-        className="hudini-logo"
-        src={HudiniLogo}
-        alt="logo"
-      /></div>
+        <img className="hudini-logo" src={HudiniLogo} alt="logo" />
+      </div>
       <ul className="module-list">
         <li
-          className={`module-tile ${activeIndex === 0 ? "active" : ""}`}
+          className={`module-tile ${activeIndex === 0 ? 'active' : ''}`}
           onClick={() => handleTileClick(0)}
         >
           <DashboardIcon />
           <p>Dashboard</p>
         </li>
         <li
-          className={`module-tile ${activeIndex === 1 ? "active" : ""}`}
+          className={`module-tile ${activeIndex === 1 ? 'active' : ''}`}
           onClick={() => handleTileClick(1)}
         >
           <ApartmentIcon />
           <p>Properties</p>
         </li>
         <li
-          className={`module-tile ${activeIndex === 2 ? "active" : ""}`}
+          className={`module-tile ${activeIndex === 2 ? 'active' : ''}`}
           onClick={() => handleTileClick(2)}
         >
           <LocationOnIcon />
           <p>Hotel</p>
         </li>
         <li
-          className={`module-tile ${activeIndex === 4 ? "active" : ""}`}
+          className={`module-tile ${activeIndex === 4 ? 'active' : ''}`}
           onClick={() => handleTileClick(4)}
         >
           <AnalyticsIcon />
           <p>Analytics</p>
         </li>
         <li
-          className={`module-tile ${activeIndex === 5 ? "active" : ""}`}
+          className={`module-tile ${activeIndex === 5 ? 'active' : ''}`}
           onClick={() => handleTileClick(5)}
         >
           <DiningIcon />
           <p>Services</p>
         </li>
         <li
-          className={`module-tile ${activeIndex === 6 ? "active" : ""}`}
+          className={`module-tile ${activeIndex === 6 ? 'active' : ''}`}
           onClick={() => handleTileClick(6)}
         >
           <PersonIcon />
           <p>Users</p>
         </li>
         <li
-          className={`module-tile ${activeIndex === 3 ? "active" : ""}`}
-          onClick={() => {
-            handleTileClick(3); 
-            handleConfigClick(); 
-          }}
+          className={`module-tile ${activeIndex === 3 ? 'active' : ''}`}
+          onClick={() => handleTileClick(3)}
         >
           <DataObjectIcon />
           <p>Configurations</p>
