86ea780bd34771f1e63e6487e463512f6edaad81
Athira S L <athira.sl@hudini.io>
2024-09-24 12:43:22 +0530

    selected field changes

--- configdemo/src/components/screens/fieldSelected.js
+++ configdemo/src/components/screens/fieldSelected.js
@@ -1,6 +1,6 @@
-// import React from "react";
-// import "./styles.scss";
-// import DeleteIcon from "@mui/icons-material/Delete";
+// import React from 'react';
+// import './styles.scss';
+// import DeleteIcon from '@mui/icons-material/Delete';
 
 // const FieldSelected = ({ data = [], setData }) => {
 //   const removeItem = (hotelId, moduleName, submoduleName) => {
@@ -30,9 +30,9 @@
 //               };
 //             }
 //           }
-//           return null; 
+//           return null;
 //         }
-//         return hotel; 
+//         return hotel;
 //       })
 //       .filter(Boolean);
 
@@ -43,10 +43,10 @@
 //     const updatedData = data
 //       .map((hotel) => {
 //         if (hotel.hotelId === hotelId) {
-//           const { [key]: _, ...remainingKeys } = hotel; 
+//           const { [key]: _, ...remainingKeys } = hotel;
 //           return {
 //             ...remainingKeys,
-//             hotelId: hotel.hotelId, 
+//             hotelId: hotel.hotelId,
 //             name: hotel.name,
 //             modules: hotel.modules,
 //           };
@@ -70,10 +70,10 @@
 //                   ...mod,
 //                   submodules: mod.submodules.map((sub) => {
 //                     if (sub.name === submoduleName) {
-//                       const { [key]: _, ...remainingKeys } = sub; 
+//                       const { [key]: _, ...remainingKeys } = sub;
 //                       return {
 //                         ...remainingKeys,
-//                         name: sub.name, 
+//                         name: sub.name,
 //                       };
 //                     }
 //                     return sub;
@@ -93,16 +93,16 @@
 
 //   const handleSubmit = () => {
 //     if (data.length === 0) {
-//       alert("Please add items to submit!");
+//       alert('Please add items to submit!');
 //       return;
 //     }
 
 //     const textData = JSON.stringify(data, null, 2);
-//     const blob = new Blob([textData], { type: "text/plain" });
+//     const blob = new Blob([textData], { type: 'text/plain' });
 //     const url = URL.createObjectURL(blob);
-//     const a = document.createElement("a");
+//     const a = document.createElement('a');
 //     a.href = url;
-//     a.download = "selected_data.tsx"; 
+//     a.download = 'GET_CONFIGURATION.tsx';
 //     document.body.appendChild(a);
 //     a.click();
 //     document.body.removeChild(a);
@@ -116,16 +116,13 @@
 //   return (
 //     <div className="field-selected">
 //       <h1>Selected Configuration</h1>
-//       <div className="selected-json">
 //       <pre className="selected-json-container">
 //         {JSON.stringify(data, null, 2)}
 //       </pre>
-//       </div>
 //       <ul>
 //         {data.map((hotel, hotelIndex) => (
 //           <li key={hotelIndex}>
-//             <div className="hotel-id-info">
-//             <div className="hotel-info">
+//             <div>
 //               {`Hotel: ${hotel.name}, ID: ${hotel.hotelId}`}
 //               <button
 //                 className="remove-btn"
@@ -136,9 +133,11 @@
 //             </div>
 
 //             {Object.keys(hotel)
-//               .filter((key) => !["hotelId", "name", "modules", "title"].includes(key)) 
+//               .filter(
+//                 (key) => !['hotelId', 'name', 'modules', 'title'].includes(key)
+//               )
 //               .map((key) => (
-//                 <div key={key} className="hotel-info">
+//                 <div key={key}>
 //                   {`${key}: ${hotel[key]}`}
 //                   <button
 //                     className="remove-btn"
@@ -148,24 +147,21 @@
 //                   </button>
 //                 </div>
 //               ))}
-//             </div>
+
 //             {hotel.modules &&
 //               hotel.modules.length > 0 &&
 //               hotel.modules.map((module, moduleIndex) => (
-//                 <div key={moduleIndex} className="hotel-sub-info">
-//                   <div className="module-info">
-//                   {`Module: ${module.name}`}
+//                 <div key={moduleIndex}>
+//                   <span>{`Module: ${module.name}`}</span>
 //                   <button
 //                     className="remove-btn"
 //                     onClick={() => removeItem(hotel.hotelId, module.name)}
 //                   >
 //                     <DeleteIcon style={{ fontSize: 18 }} />
 //                   </button>
-//                   </div>
 //                   {module.submodules.map((submodule, submoduleIndex) => (
-//                     <div key={submoduleIndex} className="submodule-info" >
-//                     <div>
-//                       {`Submodule: ${submodule.name}`}
+//                     <div key={submoduleIndex}>
+//                       <span>{`Submodule: ${submodule.name}`}</span>
 //                       <button
 //                         className="remove-btn"
 //                         onClick={() =>
@@ -174,10 +170,9 @@
 //                       >
 //                         <DeleteIcon style={{ fontSize: 18 }} />
 //                       </button>
-//                     </div>
-                            
-//                        {Object.keys(submodule).map((key) => {
-//                         if (key !== "name") {
+
+//                       {Object.keys(submodule).map((key) => {
+//                         if (key !== 'name') {
 //                           return (
 //                             <div key={key}>
 //                               {`${key}: ${submodule[key]}`}
@@ -212,7 +207,6 @@
 // };
 
 // export default FieldSelected;
-
 
 
 import React from "react";
