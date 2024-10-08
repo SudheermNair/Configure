bcb7cec8d55e4c9acf37c241a9245a8af8e5ceb0
Aswin U <aswin.u@hudini.io>
2024-10-08 11:50:15 +0530

    bugFix poc

--- configdemo/src/components/screens/fieldSelected.js
+++ configdemo/src/components/screens/fieldSelected.js
@@ -180,6 +180,7 @@ const FieldSelected = ({ data = [], setData }) => {
     <div className="field-selected">
       <div className='headingAndBtn'>
       <h3>Selected Configuration
+      </h3>
       <button onClick={copyObject} className="copyBtn">
                   {copyButtonText === "Copy" ? (
                     <ContentCopyIcon />
@@ -187,7 +188,7 @@ const FieldSelected = ({ data = [], setData }) => {
                     <DoneIcon />
                   )}
                   {copyButtonText}
-                </button></h3>
+                </button>
                 
       </div>
       <div className="selected-json">
